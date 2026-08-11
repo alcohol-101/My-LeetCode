fbaf6d87cc2b1a9b966547cbd35e345a2b329417
alcohol-101 <1958853147@qq.com>
2026-08-11 20:06:02 +0800

    Time: 27 ms (9.15%), Space: 57.4 MB (5.58%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int n1=stoi(num1);
-        int n2=stoi(num2);
-        int sum=n1+n2;
-        string s=to_string(sum);
+       int carry=0;
+       string s;
+       
+       int i=num1.size()-1;
+       int j=num2.size()-1;
+       while(i>=0||j>=0||carry>0){
+        int a= i>=0?num1[i]-'0':0;
+        int b= j>=0?num2[j]-'0':0;
+        int cur=a+b+carry;
+        carry=cur/10;
+        s=char(cur%10+'0')+s;
+        i--;j--;
+       }
         return s;
     }
 };
