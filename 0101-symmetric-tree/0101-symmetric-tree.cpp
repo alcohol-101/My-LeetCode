251c06dfeefda5512626866afb84e8b965b4304f
alcohol-101 <1958853147@qq.com>
2026-08-11 19:33:15 +0800

    Time: 0 ms (100%), Space: 18.2 MB (36.63%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -12,45 +12,14 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        if(root->left==nullptr&&root->right==nullptr)return true;
-        if(root->left==nullptr&&root->right!=nullptr)return false;
-        else if(root->left!=nullptr&&root->right==nullptr)return false;
-        deque<TreeNode *>q;
-        q.push_back(root->left);
-        q.push_back(root->right);
-        if(root->left->val!=root->right->val)return false;
-        while(q.size()!=0){
-            if (q.size() % 2 != 0) return false;
-            deque<TreeNode *>l,r;
-            while(!q.empty()){
-                TreeNode *ll=q.front()->left,*lr=q.front()->right,
-                         *rl=q.back()->left,*rr=q.back()->right;
-                bool first=false,end=false;
-                if(!ll&&!rr)first=true;
-                else if(!ll||!rr)return false;
-                if(!lr&&!rl)end=true;
-                else if(!rl||!lr)return false;
-                if(!first){
-                    if(ll->val!=rr->val)return false;
-                }
-                if(!end){
-                    if(rl->val!=lr->val)return false;
-                }
-                if(ll)l.push_back(ll);
-                if(lr)l.push_back(lr);
-               
-                q.pop_front();
-                
-                if(rr)r.push_back(rr);
-                if(rl)r.push_back(rl);
-                q.pop_back();
-            }
-            
-            reverse(r.begin(),r.end());
-            q.insert(q.end(),l.begin(),l.end());
-            q.insert(q.end(),r.begin(),r.end());
-        }
+        return isMirror(root->left,root->right);
+    }
 
-        return true;
+    bool isMirror(TreeNode*l,TreeNode*r){
+        if(!l&&!r)return true;//均为空
+        else if(!l||!r)return false;//半是空
+        return (l->val==r->val)//均非空
+                &&isMirror(l->left,r->right)
+                &&isMirror(l->right,r->left);
     }
 };
