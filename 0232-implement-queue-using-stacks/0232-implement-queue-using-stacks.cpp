eee0663d63268323fb3488b96fca25b09d670386
alcohol-101 <1958853147@qq.com>
2026-08-13 19:58:53 +0800

    Time: 0 ms (100%), Space: 9.9 MB (48.39%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,23 +1,39 @@
 class MyQueue {
+private:
+    stack<int>a,b;
+    void change(stack<int>&x,stack<int>&y){
+        while(!x.empty()){
+            int tmp=x.top();
+            x.pop();
+            y.push(tmp);
+        }
+    }
 public:
     MyQueue() {
         
     }
     
     void push(int x) {
-        
+        a.push(x);
     }
     
     int pop() {
-        
+        change(a,b);
+        int tmp=b.top();
+        b.pop();
+        change(b,a);
+        return tmp;
     }
     
     int peek() {
-        
+        change(a,b);
+        int tmp=b.top();
+        change(b,a);
+        return tmp;
     }
     
     bool empty() {
-        
+        return a.empty();
     }
 };
 
