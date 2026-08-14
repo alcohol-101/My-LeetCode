372594b866a257433be52206f3cde7e0997aa16c
alcohol-101 <1958853147@qq.com>
2026-08-14 21:59:51 +0800

    Time: 3 ms (92.68%), Space: 23.6 MB (23.07%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size()==0)return {{}};
+        if(intervals.size()==1)return vector<vector<int>>{intervals[0]};
+        vector<vector<int>>result;
+        sort(intervals.begin(),intervals.end());
+        result.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++){
+            if(result.back()[1]>=intervals[i][0]){
+                result.back()[1]=max(result.back()[1],intervals[i][1]);
+            }
+            else result.push_back(intervals[i]);
+        }
+    
+        return result;
+    }
+    
+};
