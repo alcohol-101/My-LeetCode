8c17a24598fc9a2097a31d00337624fc98fbbe90
alcohol-101 <1958853147@qq.com>
2026-09-18 17:40:17 +0800

    Time: 16 ms (6.96%), Space: 14.9 MB (63.43%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -18,29 +18,18 @@ class Solution {
 public:
     Node* copyRandomList(Node* head) {
         if(!head)return NULL;
-        vector<Node*>old_node;
-        Node *now=head;
+        unordered_map<Node*,Node*>mp;
+        Node* now=head;
         while(now){
-            old_node.push_back(now);
+            mp[now]=new Node(now->val);
             now=now->next;
         }
-        vector<Node*>new_node;
-        new_node.resize(2*old_node.size());
-        unordered_map<Node*,int>old_index;
-        //初步构建
-        for(int i=0;i<2*old_node.size();i+=2){
-            new_node[i]=old_node[i/2];
-            new_node[i+1]=new Node(old_node[i/2]->val);
-            old_index[new_node[i]]=i;
+        now=head;
+        while(now){
+            if(now->next)mp[now]->next=mp[now->next];
+            if(now->random)mp[now]->random=mp[now->random];
+            now=now->next;
         }
-        //处理next
-         for(int i=1;i<2*old_node.size();i+=2){
-            if(i!=2*old_node.size()-1)new_node[i]->next=new_node[i+2];
-            if(new_node[i-1]->random){
-                int ran=old_index[new_node[i-1]->random];
-                new_node[i]->random=new_node[ran+1];
-            }
-        }
-        return new_node[1];
+        return mp[head];
     }
 };
