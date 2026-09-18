67b16e151ff5a2099df2d3f65854abb12c2651fb
alcohol-101 <1958853147@qq.com>
2026-09-18 16:57:33 +0800

    Time: 0 ms (100%), Space: 11.1 MB (29.36%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -13,31 +13,18 @@ public:
     ListNode* swapPairs(ListNode* head) {
         if(!head)return nullptr;
         if(head->next==nullptr)return head;
-        ListNode*now=head;
-        ListNode*pre=new ListNode();
+        ListNode* pre=new ListNode();
+        ListNode* ans=pre;
         pre->next=head;
-        ListNode*ans=pre;
-        bool isout=false;
-        while(now!=nullptr){
-            if(now->next==nullptr)break;//奇数，直接跳出
-            if(now->next->next==nullptr){
-                isout=true;
-                break;
-            }
-            ListNode* tmp=now->next->next;
-            ListNode* tmp_next=now->next;
-            pre->next=tmp_next;
-            now->next->next=now;
-            now->next=tmp;
+        while(pre->next&&pre->next->next){
+            ListNode *first=pre->next,*second=pre->next->next;
+            pre->next=second;
+            first->next=second->next;
+            second->next=first;
             
-            pre=now;
-            now=tmp;
+            pre=first;
         }
-        if(isout){//偶数，翻转最后的一对
-            pre->next=now->next;
-            now->next->next=now;
-            now->next=nullptr;
-        }
+        
         return ans->next;
     }
 };
